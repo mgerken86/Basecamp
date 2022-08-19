0c0696f57ede1e1e07a2c8a6bf90dd76e3260485
Mark Gerken <mgerken86@gmail.com>
2022-08-18 20:13:11 -0600

    urls.py '/'

--- basecamp/urls.py
+++ basecamp/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main_app.urls')),
+    path('/', include('main_app.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     # path('rentals', include('main_app.urls')),
     # path('events',  include('main_app.urls')),
